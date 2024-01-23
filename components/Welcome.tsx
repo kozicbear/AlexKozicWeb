3837836f49135d2b60ca8c251c9c27dea03f6661
Alexander Kozic <akozic@bowdoin.edu>
2024-01-23 18:58:52 -0500

    Completed rough outline of welcome, started general page styling

--- components/Welcome.tsx
+++ components/Welcome.tsx
@@ -5,23 +5,28 @@ const Welcome = () => {
   return (
     <section className="max-container padding-container flex flex-col
     gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row border-2 
-    border-red-500">
+    bg-stone-200 border-red-500">      
+      <div>
+        {/* TODO: Ensure this new styling works well with different screen sizes */}
+        {/* <div className="relative z-20 flex flex-1 flex-col xl:w-1/2"> */}
 
-      {/* LEFT */}
-      {/* TODO: Replace this image with my photo */}
-      <div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
+        <div className="inline-block pl-40 pr-40">
+          <h1 className="bold-52 lg:bold-50">Alexander Kozic</h1>
+          <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px]">
+            Welcome to my personal Website
+          </p>
+        </div>
+
         <Image
-          src="/camp.svg"
-          alt="camp"
-          width={50}
-          height={50}
-          className='absolute left-[-5px] top-[-30px] w-10 lg:w-[50px]'
-        />
-        <h1 className="bold-52 lg:bold-88">Alexander Kozic</h1>
-        <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px]">
-          Welcome to my personal Website
-        </p>
+            src="/MeWCat.png"
+            alt="catAndI"
+            width={300}
+            height={300}
+            className='inline-block'
+          />
+
       </div>
+      
     </section>
   )
 }

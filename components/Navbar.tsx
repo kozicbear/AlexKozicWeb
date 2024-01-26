85a6ef983245fd5d734e57a890cfa6271674ca6c
Alexander Kozic <akozic@bowdoin.edu>
2024-01-25 22:56:33 -0500

    Adjusted styling and content of navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,33 +1,34 @@
-import { NAV_LINKS } from "@/constants"
-import Image from "next/image"
-import Link from "next/link"
-import { TbHome } from "react-icons/tb";
+import { NAV_LINKS } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import { TbHome, TbMenu2 } from "react-icons/tb";
 
 const Navbar = () => {
   return (
-    <nav className="bg-stone-50 flexBetween max-container 
-    padding-container relative z-30 py-5">
-      <TbHome size={30}/>
-      <ul className="hidden h-full gap-12 lg:flex items-center justify-center">
-          {NAV_LINKS.map((link) => (
-            <Link href={link.href} key={link.key}
-            className="regular-16 text-gray-50 flexCenter cursor-pointer
-             pb-1.5  transition-all hover:font-bold">
-              {link.label}
-            </Link>
-          ))}
-        </ul>
+    <nav className="w-full fixed top-0 z-30 bg-stone-50 py-5 max-container padding-container">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center flex-grow h-full">
+          <TbHome size={30} />
+          <ul className="hidden h-full ml-auto gap-12 sm:flex items-center">
+            {NAV_LINKS.map((link) => (
+              <Link
+                href={link.href}
+                key={link.key}
+                className="cursor-pointer flexCenter regular-16 text-gray-50 transition-all hover:text-orange-300"
+              >
+                {link.label}
+              </Link>
+            ))}
+          </ul>
+        </div>
 
-        <Image
-          src="menu.svg"
-          alt="menu"
-          width={32}
-          height={32}
-          className="inline-block cursor-pointer lg:hidden"
-        >
-        </Image>
+        <TbMenu2
+          size={30}
+          className="inline-block cursor-pointer sm:hidden"
+        />
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;

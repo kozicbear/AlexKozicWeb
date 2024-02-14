83db2675f3799167d62773aa6e61191961597905
Alexander Kozic <akozic@bowdoin.edu>
2024-02-13 22:24:25 -0500

    Pushing carousel attempt with react-responsive-carousel

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,17 +1,23 @@
+'use client'
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { PROJECTS } from "@/constants";
-import Image from 'next/image';
-
 
 const Projects = () => {
   return (
     <section className='2xl:max-container relative flex flex-col py-10 
     lg:mb-10 lg:py-20 xl:mb-20 bg-stone-100'>
-      <div>
- 
-      </div>
-      
+      <Carousel useKeyboardArrows={true}>
+        {PROJECTS.map(Project => (
+          <div className="slide">
+            <img 
+              alt={Project.alt} 
+              src={Project.src} 
+              key={Project.id} 
+            />
+          </div>
+        ))}
+      </Carousel>
     </section>
   )
 }

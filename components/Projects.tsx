e1eab7d3a69098e83a54835f852fa478db9b6517
Alexander Kozic <akozic@bowdoin.edu>
2024-02-21 23:40:56 -0500

    Added project descriptions to carousel

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -5,21 +5,36 @@ import { PROJECTS } from "@/constants";
 
 const Projects = () => {
   return (
-    <section className='2xl:max-container relative flex flex-col py-10 
-    lg:mb-10 lg:py-20 xl:mb-20 bg-stone-100'>
-      <div className="max-w-4xl mx-auto">
-        <Carousel autoPlay useKeyboardArrows={true} infiniteLoop className="text-center">
-          {PROJECTS.map(Project => (
-            <div className="slide" key={Project.id}>
-              <img 
-                alt={Project.alt} 
-                src={Project.src} 
-                className="mx-auto max-w-full h-auto" // Center images and ensure they fit within the container
-              />
-            </div>
-          ))}
-        </Carousel>
+    <section className='2xl:max-container relative flex flex-col padding-container 
+    lg:mb-10 lg:py-20 xl:mb-20 bg-stone-100 px-36'>
+
+      <div className="pl-12 pb-4 bold-32 lg:bold-50 text-orange-200">
+        Projects
       </div>
+
+      <Carousel useKeyboardArrows={true} infiniteLoop className="text-center px-16">
+        {PROJECTS.map(Project => (
+          <div className="slide" key={Project.id}>
+            <div className="flex items-center flex-row">
+              <div className="flex-shrink-0 w-1/2">
+                <img 
+                  alt={Project.alt} 
+                  src={Project.src} 
+                  className="max-w-full h-auto rounded-lg" // Ensure the image fits within the container
+                />
+              </div>
+              
+              <div className="w-1/2 flex-grow">
+                <div>{Project.name}</div>
+                <div>{Project.year}</div>
+                <div>{Project.skills}</div>
+                <div>{Project.description}</div>
+              </div>
+            </div>
+            
+          </div>
+        ))}
+      </Carousel>
     </section>
   )
 }
--- constants/index.tsx
+++ constants/index.tsx
@@ -7,7 +7,40 @@ export const NAV_LINKS = [
 ];
 
 export const PROJECTS = [
-  {id: 1, src: "/NOTAMWeb.jpeg", alt:"NOTAM Web Viewer"},
-  {id: 2, src: "/MUSESLAM.jpeg", alt:"MUSESLAM"},
-  {id: 3, src: "/AlexWeb.jpeg", alt:"Alex Website"},
+  {
+    id: 1, 
+    src:"/NOTAMWeb.jpeg", 
+    alt:"NOTAM Web Viewer", 
+    name:"NOTAM Web Viewer", 
+    year:"2023",
+    skills:"React, Typescript, Web Development",
+    description:`Upgraded ForeFlight's internal tooling from a dated
+      desktop application to a modern web application using TypeScript 
+      and React, expediting internal teams' ability to find and scrub
+      NOTAMs (Notices to Airmen), a mission-critical product`
+  },
+  {
+    id: 2, 
+    src: "/MUSESLAM.jpeg", 
+    alt:"MUSESLAM",
+    name:"MUSESLAM", 
+    year:"2023",
+    skills:"React, Typescript, Web Development",
+    description:`Upgraded ForeFlight's internal tooling from a dated
+      desktop application to a modern web application using TypeScript 
+      and React, expediting internal teams' ability to find and scrub
+      NOTAMs (Notices to Airmen), a mission-critical product`
+  },
+  {
+    id: 3, 
+    src: "/AlexWeb.jpeg", 
+    alt:"Alex Website",
+    name:"Alex's Website",
+    year:"2023",
+    skills:"React, Typescript, Web Development",
+    description:`Upgraded ForeFlight's internal tooling from a dated
+      desktop application to a modern web application using TypeScript 
+      and React, expediting internal teams' ability to find and scrub
+      NOTAMs (Notices to Airmen), a mission-critical product`
+  },
 ];

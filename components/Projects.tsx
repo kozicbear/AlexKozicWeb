753b1956a9af19ef3ad8e331959d347fd0ccefb4
Alexander Kozic <akozic@bowdoin.edu>
2024-03-02 23:02:12 -0500

    Completed xl styling of projects section

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,43 +1,81 @@
-'use client'
+"use client"
+
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { PROJECTS } from "@/constants";
 import SkillBox from "./SkillBox";
+import { TbChevronRight, TbChevronLeft } from "react-icons/tb";
+import { SetStateAction, useState } from 'react';
 
 const Projects = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const handlePrevClick = () => {
+    setCurrentSlide(currentSlide - 1);
+  };
+
+  const handleNextClick = () => {
+    setCurrentSlide(currentSlide + 1);
+  };
+
+  const handleSlideChange = (index: SetStateAction<number>) => {
+    setCurrentSlide(index);
+  };
+
   return (
     <section className='2xl:max-container relative flex flex-col padding-container 
-    py-20 bg-stone-100 px-36'>
-
-      <div className="pl-12 pb-5 bold-32 lg:bold-50 text-gray-600">
+    py-20 bg-stone-100 px-36 text-gray-600'>
+      <div className="pl-12 pb-5 bold-32 lg:bold-5">
         Projects
       </div>
 
-      <Carousel autoPlay={true} showIndicators={false} showThumbs={false} infiniteLoop={true} 
-      interval={7000} transitionTime={1000} className="px-16">
-        {PROJECTS.map(Project => (
-          <div className="slide rounded-lg shadow-sm bg-white p-6 m-1" key={Project.id}>
-            <div className="flex flex-row">
-              <div className="flex-shrink-0 w-1/2 mr-2">
-                <img 
-                  alt={Project.alt} 
-                  src={Project.src} 
-                  className="max-w-full h-auto rounded-lg"
-                />
-              </div>
-              
-              <div className="w-1/2 flex-grow p-2 ml-2 text-left text-gray-500">
-                <div className="bold-40 text-gray-600 pb-2">{Project.name}</div>
-                <div className="pb-2">{Project.type}</div>
-                <SkillBox skills={Project.skills}></SkillBox>
-                <div>{Project.description}</div>
+      <div className="flex flex-row items-center justify-center relative mx-[-50px]">
+        <button onClick={handlePrevClick} style={{ fontSize: 20, padding: '5px 20px', margin: '5px 0px' }}>
+          <TbChevronLeft size={30} className="font-bold"/>
+        </button>
+        
+        <Carousel
+          autoPlay={true}
+          showIndicators={false}
+          showArrows={false}
+          showThumbs={false}
+          infiniteLoop={true}
+          interval={7000}
+          transitionTime={1000}
+          selectedItem={currentSlide}
+          onChange={handleSlideChange}
+        >
+          {PROJECTS.map(Project => (
+            <div className="slide rounded-lg shadow-sm bg-white p-6 m-1" key={Project.id}>
+              <div className="flex flex-row">
+                <div className="flex-shrink-0 w-1/2 mr-2">
+                  <img 
+                    alt={Project.alt} 
+                    src={Project.src} 
+                    className="max-w-full h-auto rounded-lg"
+                  />
+                </div>
+                
+                <div className="w-1/2 flex-grow p-2 ml-2 text-left text-gray-500">
+                  <div className="bold-40 text-gray-600 pb-2">{Project.name}</div>
+                  <div className="pb-2">{Project.type}</div>
+                  <SkillBox skills={Project.skills}></SkillBox>
+                  <div>{Project.description}</div>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-      </Carousel>
+          ))}
+        </Carousel>
+        
+        <div className="">
+          <button onClick={handleNextClick} style={{ fontSize: 20, padding: '5px 20px', margin: '5px 0px' }}>
+            <TbChevronRight size={30} className="font-bold"/>
+          </button>
+        </div>
+      </div>
     </section>
   )
 }
 
-export default Projects
+export default Projects;
+
